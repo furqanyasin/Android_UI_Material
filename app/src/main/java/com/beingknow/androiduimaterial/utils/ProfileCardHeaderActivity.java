a1a13bb149984a99a9f38f77670c90bebb5db88f
furqanyasin <furqanyasin1997@gmail.com>
2021-05-26 00:26:51 +0500

    Part 2-ProfileCardHeaderActivity

---
+++ app/src/main/java/com/beingknow/androiduimaterial/utils/ProfileCardHeaderActivity.java
@@ -0,0 +1,16 @@
+package com.beingknow.androiduimaterial.utils;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.beingknow.androiduimaterial.R;
+
+public class ProfileCardHeaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_card_header);
+    }
+}
